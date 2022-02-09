00e0dfb02c1e61f443b39284d89a55789dc9c611
Pradeepta Kumar Sahu <psahu@nasuni.com>
2022-02-09 20:13:23 +0530

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,19 @@
+
+################### Lambda Provisioning Specific Variables ###################
+variable "runtime" {
+  default = "python3.6"
+}
+variable "region" {
+  default = "us-east-2"
+}
+variable "aws_profile" {
+  default = "nasuni"
+}
+variable "admin_secret" {
+  default = "nasuni-labs-os-admin"
+}
+variable "internal_secret" {
+  default = "nac-es-internal"
+}
+
+#########################################
