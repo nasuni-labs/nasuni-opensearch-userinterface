7089ce7819e832da3282879ae0a7b2ac48a8db8b
John Capello <87036971+jcapello-nasuni@users.noreply.github.com>
2022-02-17 08:45:44 -0500

    API Gateway integration (#1)
    * Integrate API Gateway provisioning for get volume lambda
    
    * API Gateway for Search Lambda
    
    Co-authored-by: Pradeepta Kumar Sahu <psahu@nasuni.com>

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ locals {
   lambda_code_extension          = ".py"
   handler                        = "lambda_handler"
   resource_name_prefix           = "nasuni-labs"
+  stage_name                     = var.stage_name
 
 }
 
@@ -27,6 +28,7 @@ resource "aws_lambda_function" "lambda_function_search_es" {
   runtime          = var.runtime
   filename         = "${local.lambda_code_files}.zip"
   function_name    = "${local.resource_name_prefix}-${local.lambda_code_file_name_SearchUI}-${random_id.unique_SearchUI_id.dec}"
+
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   timeout          = 20
 
@@ -165,7 +167,7 @@ EOF
     Version         = "V 0.1"
   }
 }
-
+ 
 resource "aws_iam_role_policy_attachment" "lambda_logging" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.lambda_logging.arn
@@ -212,6 +214,7 @@ resource "aws_iam_policy" "GetSecretValue_access" {
   path        = "/"
   description = "IAM policy for accessing secretmanager from a lambda"
 
+
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -256,6 +259,7 @@ resource "aws_iam_role_policy_attachment" "CloudWatchFullAccess" {
 }
 
 data "aws_iam_policy" "AmazonESFullAccess" {
+
   arn = "arn:aws:iam::aws:policy/AmazonESFullAccess"
 }
 
@@ -286,14 +290,218 @@ resource "aws_iam_role_policy_attachment" "AmazonOpenSearchServiceReadOnlyAccess
 ################### END - Lambda Role and Policies  ####################################################
 
 
-################### START - API Gateway setup for Lambda ####################################################
+################### START - API Gateway setup for Get Volume Lambda and Search ES Lambda ####################################################
 
 
-################### END - API Gateway setup for Lambda ####################################################
+#Creating Rest API gateway
+resource "aws_api_gateway_rest_api" "SearchES-API" {
+  name        = "${local.resource_name_prefix}-${local.lambda_code_files}-${random_id.unique_SearchUI_id.dec}"
+  description = "API created for exposing Lambda Functions for fetching Volumes and Search ES Data"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+  depends_on = [
+    aws_lambda_function.lambda_function_get_es_volumes,
+    aws_lambda_function.lambda_function_search_es
+  ]
+}
+################### START - API Gateway setup for Get Volume Lambda ####################################################
+
+#Creating Resource for Rest API Gateway created
+resource "aws_api_gateway_resource" "APIresourceForVolumeFetch" {
+
+  path_part   = "es-volume"
+  parent_id   = aws_api_gateway_rest_api.SearchES-API.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.SearchES-API.id
+depends_on = [
+  aws_api_gateway_rest_api.SearchES-API
+]
+}
+
+#Creating Method for API Gateway Created
+resource "aws_api_gateway_method" "APImethodForLambdaFunction" {
+  rest_api_id   = aws_api_gateway_rest_api.SearchES-API.id
+  resource_id   = aws_api_gateway_resource.APIresourceForVolumeFetch.id
+  http_method   = "GET"
+  authorization = "NONE"
+depends_on = [
+  aws_api_gateway_resource.APIresourceForVolumeFetch
+]
+}
+
+#Integrating the API Gateway with the Lambda function to deploy
+resource "aws_api_gateway_integration" "IntegratingLambdaFunctionWithAPIgateway" {
+  rest_api_id             = aws_api_gateway_rest_api.SearchES-API.id
+  resource_id             = aws_api_gateway_resource.APIresourceForVolumeFetch.id
+  http_method             = aws_api_gateway_method.APImethodForLambdaFunction.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda_function_get_es_volumes.arn}/invocations"
+
+  request_templates = {
+    "application/xml" = <<EOF
+{
+   "body" : $input.json('$')
+}
+EOF
+  }
+depends_on = [
+  aws_api_gateway_rest_api.SearchES-API,
+  aws_api_gateway_resource.APIresourceForVolumeFetch,
+  aws_api_gateway_method.APImethodForLambdaFunction
+]
+
+}
 
 
-################### START - Deploy SearchUI Web  ####################################################
+
+#Specifying integration response of the lambda function with the API gateway for fetching volume
+resource "aws_api_gateway_integration_response" "APIintegrationResponseOfLambdafunction" {
+  rest_api_id = aws_api_gateway_rest_api.SearchES-API.id
+  resource_id = aws_api_gateway_resource.APIresourceForVolumeFetch.id
+  http_method = aws_api_gateway_method.APImethodForLambdaFunction.http_method
+  status_code = aws_api_gateway_method_response.APIgatewayMethodResponse.status_code
+
+  response_templates = {
+    "application/json" = ""
+  }
+ 
+}
+#Specifying method response for integrating API gateway with Lambda function for fetching volume
+resource "aws_api_gateway_method_response" "APIgatewayMethodResponse" {
+  rest_api_id = aws_api_gateway_rest_api.SearchES-API.id
+  resource_id = aws_api_gateway_resource.APIresourceForVolumeFetch.id
+  http_method = aws_api_gateway_method.APImethodForLambdaFunction.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+#Getting permission from Lambda function to API GAteway
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "${local.resource_name_prefix}-${local.lambda_code_files}-AllowExecutionFromAPIGateway-${random_id.unique_SearchUI_id.dec}"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.lambda_function_get_es_volumes.function_name}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:${var.region}:514960042727:${aws_api_gateway_rest_api.SearchES-API.id}/${aws_api_gateway_stage.StageTheAPIdeployed.stage_name}/GET${aws_api_gateway_resource.APIresourceForVolumeFetch.path}"
+}
+
+#Deploying the API created
+resource "aws_api_gateway_deployment" "APIdeploymentOfLambdaFunction" {
+  rest_api_id = aws_api_gateway_rest_api.SearchES-API.id
+
+  triggers = {
+
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.APIresourceForVolumeFetch.id,
+      aws_api_gateway_method.APImethodForLambdaFunction.id,
+      aws_api_gateway_integration.IntegratingLambdaFunctionWithAPIgateway.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  depends_on = [
+    aws_api_gateway_rest_api.SearchES-API,
+    aws_api_gateway_integration.IntegratingLambdaFunctionWithAPIgateway,
+    aws_api_gateway_integration.IntegratingLambdaFunctionWithSearchUI
+  ]
+}
+
+################### END - API Gateway setup for Get Volume Lambda ####################################################
 
 
-################### END - Deploy SearchUI Web ####################################################
 
+
+#Staging the Deployed API
+resource "aws_api_gateway_stage" "StageTheAPIdeployed" {
+  deployment_id = aws_api_gateway_deployment.APIdeploymentOfLambdaFunction.id
+  rest_api_id   = aws_api_gateway_rest_api.SearchES-API.id
+  stage_name    = "${local.stage_name}"
+}
+
+################### START - API Gateway setup for Search ES Lambda ####################################################
+
+#Creating resource for Search UI
+resource "aws_api_gateway_resource" "APIresourceForSearchUI" {
+
+  path_part   = "search-es"
+  parent_id   = aws_api_gateway_rest_api.SearchES-API.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.SearchES-API.id
+depends_on = [
+  aws_api_gateway_rest_api.SearchES-API
+]
+}
+
+#Creating Method for Search UI
+resource "aws_api_gateway_method" "APImethodForSearchUI" {
+  rest_api_id   = aws_api_gateway_rest_api.SearchES-API.id
+  resource_id   = aws_api_gateway_resource.APIresourceForSearchUI.id
+  http_method   = "GET"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.querystring.q"= true
+  }
+depends_on = [
+  aws_api_gateway_resource.APIresourceForSearchUI
+]
+}
+
+
+#Integrating the API Gateway with the Lambda function to deploy Search UI
+resource "aws_api_gateway_integration" "IntegratingLambdaFunctionWithSearchUI" {
+  rest_api_id             = aws_api_gateway_rest_api.SearchES-API.id
+  resource_id             = aws_api_gateway_resource.APIresourceForSearchUI.id
+  http_method             = aws_api_gateway_method.APImethodForSearchUI.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda_function_search_es.arn}/invocations"
+
+  request_templates = {
+    "application/xml" = <<EOF
+{
+   "body" : $input.json('$')
+}
+EOF
+  }
+depends_on = [
+  aws_api_gateway_rest_api.SearchES-API,
+  aws_api_gateway_resource.APIresourceForSearchUI,
+  aws_api_gateway_method.APImethodForSearchUI
+]
+
+}
+
+#Specifying method response for integrating API gateway with Search UI
+resource "aws_api_gateway_method_response" "APIgatewaySearchUIMethodResponse" {
+  rest_api_id = aws_api_gateway_rest_api.SearchES-API.id
+  resource_id = aws_api_gateway_resource.APIresourceForSearchUI.id
+  http_method = aws_api_gateway_method.APImethodForSearchUI.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+#Getting permission from Lambda function to API GAteway
+resource "aws_lambda_permission" "apigw_lambdaSearchUI" {
+  statement_id  = "${local.resource_name_prefix}-${local.lambda_code_files}-AllowExecutionFromAPIGatewayForSearchUI-${random_id.unique_SearchUI_id.dec}"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.lambda_function_search_es.function_name}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:${var.region}:514960042727:${aws_api_gateway_rest_api.SearchES-API.id}/${aws_api_gateway_stage.StageTheAPIdeployed.stage_name}/GET${aws_api_gateway_resource.APIresourceForSearchUI.path}"
+}
+################### END - API Gateway setup for Search ES Lambda ####################################################
+
+
+output "base_url_for_search_es" {
+  value = "${aws_api_gateway_deployment.APIdeploymentOfLambdaFunction.invoke_url}${aws_api_gateway_stage.StageTheAPIdeployed.stage_name}${aws_api_gateway_resource.APIresourceForSearchUI.path}"
+}
+
+output "base_url_for_get_es_volmes" {
+  value = "${aws_api_gateway_deployment.APIdeploymentOfLambdaFunction.invoke_url}${aws_api_gateway_stage.StageTheAPIdeployed.stage_name}${aws_api_gateway_resource.APIresourceForVolumeFetch.path}"
+}
+
+
+################### END - API Gateway setup for Get Volume Lambda and Search ES Lambda ####################################################
